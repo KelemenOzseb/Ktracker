71bebe2388127629c36a79a960b1a1e9d6b70a12
KelemenOzseb <kelemenozzseb@gmail.com>
2026-01-17 18:08:56 +0100

    initial commit

---
+++ Frontend/src/app/shared/shared-module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../components/login/login.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule
+  ],
+  exports: [
+    LoginComponent
+  ]
+})
+export class SharedModule {}
