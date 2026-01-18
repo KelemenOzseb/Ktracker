6f9c31fa13e4c91cb6f9aa967ce4775fbf16c06a
KelemenOzseb <kelemenozzseb@gmail.com>
2026-01-18 19:07:09 +0100

    food and macronutritients models done

--- Backend/KTrack/Entities/Models/Macronutrients.cs
+++ Backend/KTrack/Entities/Models/Macronutrients.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Entities.Helper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Models
 {
-    public class Macronutrients
+    public class Macronutrients : IIdentity
     {
+        public Macronutrients(string foodId, int carbohydrate, int protein, int fat)
+        {
+            Id = Guid.NewGuid().ToString();
+            FoodId = foodId;
+            Carbohydrate = carbohydrate;
+            Protein = protein;
+            Fat = fat;
+        }
+
+        public string Id { get; set; }
+        public string FoodId { get; set; }
+        public int Carbohydrate { get; set; }
+        public int Protein { get; set; }
+        public int Fat { get; set; }
+        public virtual Food Food { get; set; }
     }
 }
