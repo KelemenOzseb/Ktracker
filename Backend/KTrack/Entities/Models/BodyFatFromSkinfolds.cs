09aad2fa6c0e7e38dd8e720c84df1c5b84b2753c
KelemenOzseb <kelemenozzseb@gmail.com>
2026-01-18 19:06:29 +0100

    bodyFatSkinfold model done

--- Backend/KTrack/Entities/Models/BodyFatFromSkinfolds.cs
+++ Backend/KTrack/Entities/Models/BodyFatFromSkinfolds.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Entities.Helper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Models
 {
-    public class BodyFatFromSkinfolds
+    public class BodyFatFromSkinfolds : IIdentity
     {
+        public BodyFatFromSkinfolds(string userID, DateTime createdAt, double bodyFatPercentage, double bicepsSkinfold, double tricepsSkinfold, double scapularSkinfold, double hipSkinfold)
+        {
+            Id = Guid.NewGuid().ToString();
+            UserID = userID;
+            CreatedAt = createdAt;
+            BodyFatPercentage = bodyFatPercentage;
+            BicepsSkinfold = bicepsSkinfold;
+            TricepsSkinfold = tricepsSkinfold;
+            ScapularSkinfold = scapularSkinfold;
+            HipSkinfold = hipSkinfold;
+        }
+
+        public string Id { get; set; }
+        public string UserID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public double BodyFatPercentage { get; set; }
+        public double BicepsSkinfold { get; set; }
+        public double TricepsSkinfold { get; set; }
+        public double ScapularSkinfold { get; set; }
+        public double HipSkinfold { get; set; }
+        public virtual User User { get; set; }
     }
 }
